6ae58a9e7fd8c95fc95fdbfe1f7f82a3a490a186
viqa <vinicius@upoutsourcing.com>
2022-05-19 11:11:28 -0300

    x

--- cypress/integration/API-test/getToken.spec.js
+++ cypress/integration/API-test/getToken.spec.js
@@ -1,4 +1,4 @@
-describe ('API Testing', () => {
+describe('API Testing', () => {
 
     it('API Tests', () => {
 
@@ -6,64 +6,89 @@ describe ('API Testing', () => {
         cy.request({
             method: 'POST',
             url: '/auth',
-            form : true,
-            headers: {
-                'Content-Type': 'application/json', 
-            },
-            body: {
-                "username" : "admin",
-                "password" : "password123",
-            }
-        }).as('getToken').then((response)=>{
-            expect(response.status).to.eq(200)
-            cy.log(response.body)
-        }).its('body').then((body)=>{
-
-        const auth = body.token
-
-
-        //createBooking
-        cy.request({
-            method: 'POST',
-            url: '/booking',
-            form : true,
-            headers: {
-                'Content-Type': 'application/json', 
-            },
-            body: {
-                //its possible to use faker to randomize the data as well.
-                "firstname" : "Vini",
-                "lastname" : "Milani",
-                "totalprice": "100",
-                "depositpaid": "true",
-                "bookingdates": {
-                    "checkin": "2020-05-01",
-                    "checkout": "2020-05-02"
-            },
-            "additionalneeds": "Breakfast"
-        }
-    }).then((response)=>{
-        expect(response.status).to.eq(200)
-        cy.log (response.body)
-    })
-        //getBooking
-        cy.request({
-            method: 'GET',
-            url: '/booking/50', //we could ramdomize this number using js
-            form : true,
+            form: true,
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'token: ' + auth
             },
-          }).then((response)=>{
+            body: {
+                "username": "admin",
+                "password": "password123",
+            }
+        }).as('getToken').then((response) => {
             expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('bookingdates')
-            expect(response.body).to.have.property('depositpaid')
-            expect(response.body).to.have.property('firstname')
-            expect(response.body).to.have.property('lastname')
-            expect(response.body).to.have.property('totalprice')
             cy.log(response.body)
-          })
-      })
+        }).its('body').then((body) => {
+
+            const auth = body.token
+
+
+            //createBooking
+            cy.request({
+                method: 'POST',
+                url: '/booking',
+                form: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: {
+                    //its possible to use faker to randomize the data as well.
+                    "firstname": "Vini",
+                    "lastname": "Milani",
+                    "totalprice": "100",
+                    "depositpaid": "true",
+                    "bookingdates": {
+                        "checkin": "2020-05-01",
+                        "checkout": "2020-05-02"
+                    },
+                    "additionalneeds": "Breakfast"
+                }
+            }).then((response) => {
+                expect(response.status).to.eq(200)
+                cy.log(response.body)
+            }).its('body').then((body) => {
+
+                const bookID = body.bookingid
+                //getBooking
+                cy.request({
+                    method: 'GET',
+                    url: '/booking/' + bookID, //we could ramdomize this number using js
+                    form: true,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'token: ' + auth
+                    },
+                }).then((response) => {
+                    expect(response.status).to.eq(200)
+                    expect(response.body).to.have.property('bookingdates')
+                    expect(response.body).to.have.property('depositpaid')
+                    expect(response.body).to.have.property('firstname')
+                    expect(response.body).to.have.property('lastname')
+                    expect(response.body).to.have.property('totalprice')
+                    cy.log(response.body)
+                })
+
+                //updateBooking
+                cy.request({
+                    method: 'PUT',
+                    url: '/booking/' + bookID,
+                    form: true,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: {
+                        "firstname": "Vini",
+                        "lastname": "Milani",
+                        "totalprice": "100",
+                        "depositpaid": "true",
+                        "bookingdates": {
+                            "checkin": "2020-05-01",
+                            "checkout": "2020-05-02"
+                        },
+                        "additionalneeds": "Breakfast"
+                    }
+                })
+
+            })
+        })
     })
 })
